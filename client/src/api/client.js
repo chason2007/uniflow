3a2da561003238fa2591e04f668e69edf14de355
chason2007 <chasonchevyhurtis.2024cse@sece.ac.in>
2026-02-12 22:12:26 +0530

    feat: initialize Axios client for API communication with platform-specific base URL and default headers.

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { Platform } from 'react-native';
 
 // REPLACE WITH YOUR COMPUTER'S IP ADDRESS (e.g., 192.168.1.50)
-const SERVER_IP = '172.16.143.158';
+const SERVER_IP = '172.16.142.172';
 
 const API_URL = Platform.OS === 'android' && !Platform.isTV ? 'http://10.0.2.2:5000/api' : `http://${SERVER_IP}:5000/api`;
 
